4496ead24e31cf78f0ddf946f6e361f07141a2b3
kaloyan rizov <kalo282828@gmail.com>
2025-09-17 17:19:57 +0300

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,27 @@
 
 import HeroSection from "../components/homePage/hero-section";
-import ShowsSection from "../components/homePage/show-selection"; 
+import ShowsSection from "../components/homePage/show-selection";
 import AboutTheaterSection from "@/components/homePage/about-theathre";
 import CastSection from "../components/homePage/cast-section";
 import { createClient } from "../../lib/supabase/server";
 
-
-async function fetchShows() {
-  
-  const supabase = await createClient();
-  const { data: shows, error } = await supabase.from("shows").select("*");
-  return shows
-  
-}
-
 export default async function HomePage() {
-   let dataShows = await fetchShows()
+  const supabase = await createClient();
+
+  const { data: showsData } = await supabase.from("shows").select("*").limit(6);
+
+  const { data: castMembersData } = await supabase
+    .from("employees")
+    .select("id, name, role, profile_picture_URL")
+    .order("id")
+    .limit(8);
 
   return (
     <>
-      <HeroSection/>
-      <ShowsSection shows={dataShows}/>
-      <AboutTheaterSection/>
-      <CastSection/>
+      <HeroSection />
+      <ShowsSection shows={showsData ?? []} />
+      <AboutTheaterSection />
+      <CastSection castMembers={castMembersData ?? []} />
     </>
   );
 }
